4becef8d837db060149814fd32a2cd6122f8b716
xiaowansm <31873594+xiaowansm5@users.noreply.github.com>
2023-12-27 08:34:03 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT 快如闪电
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          您的私人ai助理.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
